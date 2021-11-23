8ee0e5cb5f5ec6287f975b3db2eb0b5b45f0383c
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-11-23 14:01:46 +0800

    Added landing page

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/MainApp.kt
@@ -0,0 +1,48 @@
+package ph.edu.dlsu.mobdeve.group.machineproject.keeb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+var et_username: EditText? = null
+var et_password: EditText? = null
+var btn_submit: Button? = null
+
+class MainApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_app)
+
+        et_username = findViewById(R.id.et_username)
+        et_password = findViewById(R.id.et_password)
+        btn_submit = findViewById(R.id.btn_submit)
+
+        btn_submit!!.setOnClickListener {
+            Log.i(
+                "MainApp",
+                "name: ${et_username?.text} pass:${et_password?.text}"
+            )
+
+            Toast.makeText(
+                applicationContext,
+                "name: ${et_username?.text} pass:${et_password?.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            var gotoLandingActivity = Intent(applicationContext, LandingActivity::class.java)
+
+            var bundle = Bundle()
+            bundle.putString("username", et_username!!.text.toString())
+
+            gotoLandingActivity.putExtras(bundle)
+            gotoLandingActivity.putExtra("username", et_username!!.text.toString())
+
+            startActivity(gotoLandingActivity)
+            finish()
+        }
+    }
+}
