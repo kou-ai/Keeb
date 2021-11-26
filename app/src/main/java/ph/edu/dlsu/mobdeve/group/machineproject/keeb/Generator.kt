d40785c742118844ea7ca33290eb9e41f15df566
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-11-26 13:26:46 +0800

    Updated Navbar, Added generator layout

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/Generator.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.group.machineproject.keeb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Generator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generator)
+    }
+}
