ca94fd808227612815dfeea949652cb7306ebd7a
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-11-26 13:57:17 +0800

    added generator

--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ForumActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ForumActivity.kt
@@ -17,57 +17,6 @@ class ForumActivity : Activity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Adds Progress Bar Support
-        this.window.requestFeature(Window.FEATURE_PROGRESS)
-        // Makes Progress Bar Visible
-        window.setFeatureInt(Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON)
 
-        // Use forum.xml as webview layout
-        setContentView(R.layout.forum)
-        webView = findViewById<View>(R.id.webView) as WebView
-        webView!!.settings.javaScriptEnabled = true
-
-        // Adds Zoom Control (You may not need this)
-        webView!!.settings.setSupportZoom(true)
-
-        // Enables Multi-Touch. if supported by ROM
-        webView!!.settings.builtInZoomControls = true
-
-        // Change to your own forum url
-        webView!!.loadUrl("http://smf-media.com/community/")
-        webView!!.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                // Loads only your frum domain and no others!
-                if (url.contains("smf-media.com") == true) {
-                    view.loadUrl(url)
-                    // Adds Progress Bar Support
-                    super.onPageStarted(view, url, null)
-                    findViewById<View>(R.id.progressbar).visibility = View.VISIBLE
-                    // If they are not your domain, use browser instead
-                } else {
-                    val i = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    startActivity(i)
-                }
-                return true
-            }
-
-            override fun onPageFinished(view: WebView, url: String) {
-                // Removes Progress Bar
-                findViewById<View>(R.id.progressbar).visibility = View.GONE
-                // Adds Cookies. Yummy!
-                CookieSyncManager.getInstance().sync()
-            }
-        }
-    }
-
-    override fun onBackPressed() {
-        // Enables going back history
-        if (webView!!.copyBackForwardList().currentIndex > 0) {
-            webView!!.goBack()
-        } else {
-            // Your exit alert code, or alternatively line below to finish
-            // Finishes forum activity
-            super.onBackPressed()
-        }
     }
 }
--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/Generator.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/Generator.kt
@@ -1,11 +1,46 @@
 package ph.edu.dlsu.mobdeve.group.machineproject.keeb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.Random
+
+
+var btn_generate: Button? = null
+var tv_switches: TextView? = null
+var generated_image: ImageView? = null
 
 class Generator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generator)
+
+
+    btn_generate = findViewById(R.id.btn_generate)
+    tv_switches = findViewById(R.id.tv_switches)
+    generated_image = findViewById(R.id.generated_image)
+
+    btn_submit!!.setOnClickListener {
+        val rnds = (0..10).random()
+        Log.i(
+            "Generator",
+            "Keyboard: $rnds"
+        )
+        
+        var gotoGenerator = Intent(applicationContext, Generator::class.java)
+
+        var bundle = Bundle()
+        bundle.putString("keyboard", rnds.toString())
+
+        gotoGenerator.putExtras(bundle)
+        gotoGenerator.putExtra("keyboard", rnds.toString())
+
+        startActivity(gotoGenerator)
+        finish()
+        }
     }
 }
