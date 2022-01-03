93180d4229de4c6a4d3d49adf55a656d601d0ea1
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2022-01-03 18:08:25 +0800

    Added profile layout. Updated gradle

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ProfileActivity.kt
@@ -0,0 +1,21 @@
+package ph.edu.dlsu.mobdeve.group.machineproject.keeb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ProfileActivity : AppCompatActivity() {
+
+    var btnEnd: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        btnEnd = findViewById(R.id.buttonLogout)
+        btnEnd!!.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            finish()
+        }
+    }
+}
