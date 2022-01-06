d2f52bd08e87e43f305fdc446d5c08271651e033
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2022-01-06 20:40:54 +0800

    Fixed main app background

--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/MainApp.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/MainApp.kt
@@ -32,7 +32,7 @@ class MainApp : AppCompatActivity() {
 
             addRecord()
 
-            var gotoForumActivity = Intent(applicationContext, GeneratorActivity::class.java)
+            var gotoForumActivity = Intent(applicationContext, ForumActivity::class.java)
 
             var bundle = Bundle()
             bundle.putString("username", et_username!!.text.toString())
