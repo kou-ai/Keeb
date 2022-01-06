9657b3e8a607b388f82569e5b583eaf2f38c23ed
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2022-01-06 19:14:59 +0800

    Added DB logic, fixed front end

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ItemAdapter.kt
@@ -0,0 +1,91 @@
+package ph.edu.dlsu.mobdeve.group.machineproject.keeb
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+var llMain: LinearLayout? = null
+var tvName: TextView? = null
+var tvEmail: TextView? = null
+var ivEdit: ImageView? = null
+var ivDelete: ImageView? = null
+
+
+class ItemAdapter(val context: Context, val items: ArrayList<EmpModelClass>) :
+    RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    /**
+     * Inflates the item views which is designed in the XML layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_row,
+                parent,
+                true
+            )
+        )
+
+
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val item = items.get(position)
+
+        /*holder.tvName.toString() = item.name
+        holder.tvEmail.toString() = item.email */
+
+        // Updating the background color according to the odd/even positions in list.
+        if (position % 2 == 0) {
+            holder.llMain.setBackgroundColor(
+                ContextCompat.getColor(
+                    context,
+                    R.color.purple_200
+                )
+            )
+        } else {
+            holder.llMain.setBackgroundColor(ContextCompat.getColor(context, R.color.black))
+        }
+
+
+    }
+
+    /**
+     * Gets the number of items in the list
+     */
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        // Holds the TextView that will add each item to
+
+        val llMain = view
+        var tvName = view
+        var tvEmail = view
+    }
+}
--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/MainActivity.kt
@@ -3,12 +3,18 @@ package ph.edu.dlsu.mobdeve.group.machineproject.keeb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 var etName: EditText? = null
 var etEmailId: EditText? = null
 var btnAdd: Button? = null
+var rvItemsList: RecyclerView? = null
+var tvNoRecordsAvailable: TextView? = null
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +32,32 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        setupListofDataIntoRecyclerView()
+
     }
+
+    private fun setupListofDataIntoRecyclerView() {
+
+        rvItemsList = findViewById(R.id.rvItemsList)
+
+        if (getItemsList().size > 0) {
+
+            rvItemsList!!.visibility = View.VISIBLE
+            tvNoRecordsAvailable!!.visibility = View.GONE
+
+            // Set the LayoutManager that this RecyclerView will use.
+            rvItemsList!!.layoutManager = LinearLayoutManager(this)
+            // Adapter class is initialized and list is passed in the param.
+            val itemAdapter = ItemAdapter(this, getItemsList())
+            // adapter instance is set to the recyclerview to inflate the items.
+            rvItemsList!!.adapter = itemAdapter
+        } else {
+
+            rvItemsList!!.visibility = View.GONE
+            tvNoRecordsAvailable!!.visibility = View.VISIBLE
+        }
+    }
+
 
     //Method for saving the employee records in database
     private fun addRecord() {
