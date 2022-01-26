d0cabff1d7ae27328ab1aa88d79ef888a3ca8689
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2022-01-26 17:41:46 +0800

    Changed activity to fragment

--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ForumActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ForumActivity.kt
@@ -1,31 +1,39 @@
 package ph.edu.dlsu.mobdeve.group.machineproject.keeb
 
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.webkit.CookieSyncManager
-import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.net.Uri;
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.view.Window;
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.ActivityForumBinding
 import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.ActivityMainBinding
 import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.FragmentForumBinding
+import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.FragmentHomeBinding
+import ph.edu.dlsu.mobdeve.group.machineproject.keeb.ui.home.HomeViewModel
 
 
-class ForumActivity : AppCompatActivity() {
+class ForumActivity : Fragment() {
 
-    var binding: FragmentForumBinding? = null
+    private var _binding: FragmentForumBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-    public override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentForumBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        _binding = FragmentForumBinding.inflate(inflater, container, false)
 
+        return binding.root
     }
 }
