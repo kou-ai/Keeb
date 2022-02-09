8bb5c4b4d1ed756e4e3a87646d95a893f4453f97
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2022-02-09 14:14:22 +0800

    Added firebase code

--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ProfileActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package ph.edu.dlsu.mobdeve.group.machineproject.keeb
 
+import android.app.ActionBar
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.FragmentProfileBinding
 
 class   ProfileActivity : Fragment() {
 
         private var _binding: FragmentProfileBinding? = null
+        private lateinit var firebaseAuth: FirebaseAuth
+        private lateinit var actionBar: ActionBar
         // This property is only valid between onCreateView and
         // onDestroyView.
         private val binding get() = _binding!!
@@ -25,6 +29,12 @@ class   ProfileActivity : Fragment() {
             _binding = FragmentProfileBinding.inflate(inflater, container, false)
             val btnEnd: Button = binding.buttonLogout
 
+            actionBar = requireActivity().actionBar!!
+            actionBar.title = "Profile"
+
+            firebaseAuth = FirebaseAuth.getInstance()
+            checkUser()
+
             btnEnd.setOnClickListener {
                 val intent = Intent(activity?.applicationContext, LoginActivity::class.java)
                 startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
@@ -33,7 +43,19 @@ class   ProfileActivity : Fragment() {
 
             return binding.root
         }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            val email = firebaseUser.email
+            binding.profileEmail.text = email
+        }
+        else {
+            startActivity(Intent(activity?.applicationContext, LoginActivity::class.java))
+            activity?.finish()
+        }
     }
+}
 
 
 
--- app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/RegisterActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/group/machineproject/keeb/RegisterActivity.kt
@@ -5,62 +5,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.util.Patterns
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
-import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.ActivityLoginBinding
+import ph.edu.dlsu.mobdeve.group.machineproject.keeb.databinding.ActivityRegisterBinding
 
+class RegisterActivity : AppCompatActivity() {
 
-var et_username: EditText? = null
-var et_password: EditText? = null
-var btn_submit: Button? = null
-
-class LoginActivity : AppCompatActivity() {
-
-    var binding: ActivityLoginBinding? = null
-    var btn_submit: Button? = null
-    var btn_reg: Button? = null
+    private lateinit var binding: ActivityRegisterBinding
     private lateinit var actionBar: ActionBar
     private lateinit var progressDialog: ProgressDialog
     private lateinit var firebaseAuth: FirebaseAuth
+    var btn_reg: Button? = null
     private var email = ""
     private var password = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
+        btn_reg = findViewById(R.id.btn_register2)
+
         actionBar = supportActionBar!!
-        actionBar.title = "Login"
-        btn_submit = findViewById(R.id.btn_submit)
-        btn_reg = findViewById(R.id.btn_register)
+        actionBar.title = "Sign Up"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
 
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Please wait")
-        progressDialog.setMessage("Logging in")
+        progressDialog.setMessage("Registering account")
         progressDialog.setCanceledOnTouchOutside(false)
 
         firebaseAuth = FirebaseAuth.getInstance()
-        checkUser()
 
-        btn_reg!!.setOnClickListener{
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
-
-        btn_submit!!.setOnClickListener{
+        btn_reg!!.setOnClickListener {
             validateData()
         }
     }
 
     private fun validateData() {
-        email = binding!!.etEmail.text.toString().trim()
-        password = binding!!.etPassword.text.toString().trim()
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
 
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             binding!!.etEmail.error = "Invalid Email"
@@ -68,32 +56,35 @@ class LoginActivity : AppCompatActivity() {
         else if (TextUtils.isEmpty(password)){
             binding!!.etPassword.error = "Please enter password"
         }
-        else
-            firebaseLogin()
+        else if (password.length <6){
+            binding!!.etPassword.error = "Password must be atleast 6 characters long"
+        }
+        else {
+            firebaseSignUp()
+        }
     }
 
-    private fun firebaseLogin() {
+    private fun firebaseSignUp() {
         progressDialog.show()
-        firebaseAuth.signInWithEmailAndPassword(email, password)
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
             .addOnSuccessListener {
                 progressDialog.dismiss()
                 val firebaseUser = firebaseAuth.currentUser
                 val email = firebaseUser!!.email
-                Toast.makeText(this, "Logged in as $email", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+
                 startActivity(Intent(this, MainActivity::class.java))
+                finish()
             }
             .addOnFailureListener { e->
                 progressDialog.dismiss()
-                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "sign up failed due to ${e.message}", Toast.LENGTH_SHORT).show()
             }
     }
 
-    private fun checkUser(){
-        val firebaseuser = firebaseAuth.currentUser
-        if (firebaseuser != null){
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
-
